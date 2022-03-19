d638a2582126defbaa7be4ed43ed30d6a2276539
Taimoor hassan <taimoorwarch@gmail.com>
2022-03-19 23:14:20 +0500

    safe-driver

---
+++ app.js
@@ -0,0 +1,255 @@
+    //responsive navbar
+
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
+    
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+    
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+    
+    function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+    //scroll to that section which is target in navbar
+    const sections = document.querySelectorAll('section[id]')
+    
+    function scrollActive(){
+        const scrollY = window.pageYOffset
+    
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop - 50,
+                sectionId = current.getAttribute('id')
+    
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            }else{
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            }
+        })
+    }
+    window.addEventListener('scroll', scrollActive)
+    
+   //Header color change
+
+    
+    
+    //Change th active class to the target navbar item
+    
+    document.addEventListener('DOMContentLoaded', function() {
+    
+    const selector = '.nav-link';
+    const elems = Array.from(document.querySelectorAll(selector));
+    const navigation = document.querySelector('.nav-menu');
+    
+    function makeActive(evt) {
+      const target = evt.target;
+    
+       if (!target || !target.matches(selector)) {
+         return;
+       }
+    
+      elems.forEach(elem => elem.classList.remove('active'));
+      evt.target.classList.add('active');
+    };
+    
+    navigation.addEventListener('mousedown', makeActive);
+    
+    });
+
+//procedure animation
+function reveal() {
+    var reveals = document.querySelectorAll(".innerp");
+    
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+    
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+    }
+    
+    window.addEventListener("scroll", reveal);
+    //animation about
+
+    function about() {
+      var about = document.querySelectorAll(".line-color");
+      
+      for (var i = 0; i < about.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = about[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+      
+        if (elementTop < windowHeight - elementVisible) {
+          about[i].classList.add("active");
+        } else {
+          about[i].classList.remove("active");
+        }
+      }
+      }
+      
+      window.addEventListener("scroll", about);
+      
+
+// home slider 
+ 
+let onSlide = false;
+
+window.addEventListener("load", () => {
+   autoSlide();
+
+   const dots = document.querySelectorAll(".carousel_dot");
+   for (let i = 0; i < dots.length; i++) {
+      dots[i].addEventListener("click", () => slide(i));
+   }
+
+   const buttonPrev = document.querySelector(".carousel_button__prev");
+   const buttonNext = document.querySelector(".carousel_button__next");
+   buttonPrev.addEventListener("click", () => slide(getItemActiveIndex() - 1));
+   buttonNext.addEventListener("click", () => slide(getItemActiveIndex() + 1));
+})
+
+function autoSlide() {
+   setInterval(() => {
+      slide(getItemActiveIndex() + 1);
+   }, 3000); // slide speed = 3s
+}
+
+function slide(toIndex) {
+   if (onSlide)
+      return;
+   onSlide = true;
+
+   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+   const itemActive = document.querySelector(".carousel_item__active");
+   const itemActiveIndex = itemsArray.indexOf(itemActive);
+   let newItemActive = null;
+
+   if (toIndex > itemActiveIndex) {
+      // check if toIndex exceeds the number of carousel items
+      if (toIndex >= itemsArray.length) {
+         toIndex = 0;
+      }
+
+      newItemActive = itemsArray[toIndex];
+
+      // start transition
+      newItemActive.classList.add("carousel_item__pos_next");
+      setTimeout(() => {
+         newItemActive.classList.add("carousel_item__next");
+         itemActive.classList.add("carousel_item__next");
+      }, 20);
+   } else {
+      // check if toIndex exceeds the number of carousel items
+      if (toIndex < 0) {
+         toIndex = itemsArray.length - 1;
+      }
+
+      newItemActive = itemsArray[toIndex];
+
+      // start transition
+      newItemActive.classList.add("carousel_item__pos_prev");
+      setTimeout(() => {
+         newItemActive.classList.add("carousel_item__prev");
+         itemActive.classList.add("carousel_item__prev");
+      }, 20);
+   }
+
+   // remove all transition class and switch active class
+   newItemActive.addEventListener("transitionend", () => {
+      itemActive.className = "carousel_item";
+      newItemActive.className = "carousel_item carousel_item__active";
+      onSlide = false;
+   }, {
+      once: true
+   });
+
+   slideIndicator(toIndex);
+}
+
+function getItemActiveIndex() {
+   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+   const itemActive = document.querySelector(".carousel_item__active");
+   const itemActiveIndex = itemsArray.indexOf(itemActive);
+   return itemActiveIndex;
+}
+
+function slideIndicator(toIndex) {
+   const dots = document.querySelectorAll(".carousel_dot");
+   const dotActive = document.querySelector(".carousel_dot__active");
+   const newDotActive = dots[toIndex];
+
+   dotActive.classList.remove("carousel_dot__active");
+   newDotActive.classList.add("carousel_dot__active");
+}
+
+
+
+//Text typing animation
+var TxtRotate = function(el, toRotate, period) {
+   this.toRotate = toRotate;
+   this.el = el;
+   this.loopNum = 0;
+   this.period = parseInt(period, 10) || 2000;
+   this.txt = '';
+   this.tick();
+   this.isDeleting = false;
+ };
+ 
+ TxtRotate.prototype.tick = function() {
+   var i = this.loopNum % this.toRotate.length;
+   var fullTxt = this.toRotate[i];
+ 
+   if (this.isDeleting) {
+     this.txt = fullTxt.substring(0, this.txt.length - 1);
+   } else {
+     this.txt = fullTxt.substring(0, this.txt.length + 1);
+   }
+ 
+   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+ 
+   var that = this;
+   var delta = 300 - Math.random() * 100;
+ 
+   if (this.isDeleting) { delta /= 2; }
+ 
+   if (!this.isDeleting && this.txt === fullTxt) {
+     delta = this.period;
+     this.isDeleting = true;
+   } else if (this.isDeleting && this.txt === '') {
+     this.isDeleting = false;
+     this.loopNum++;
+     delta = 500;
+   }
+ 
+   setTimeout(function() {
+     that.tick();
+   }, delta);
+ };
+ 
+ window.onload = function() {
+   var elements = document.getElementsByClassName('txt-rotate');
+   for (var i=0; i<elements.length; i++) {
+     var toRotate = elements[i].getAttribute('data-rotate');
+     var period = elements[i].getAttribute('data-period');
+     if (toRotate) {
+       new TxtRotate(elements[i], JSON.parse(toRotate), period);
+     }
+   }
+   // INJECT CSS
+   var css = document.createElement("style");
+   css.type = "text/css";
+   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+   document.body.appendChild(css);
+ };
